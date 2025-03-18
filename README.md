c9276e1d465ca54bb9237e677916aef4d9162ea4
tortaruga <jojoway77@gmail.com>
2025-03-18 16:41:35 +0100

    added footer, cleaned code

---
+++ README.md
@@ -0,0 +1,2 @@
+third version of a pointless webpage made as exercise.
+if you think this is ugly you should have seen version one!
--- app.js
+++ app.js
@@ -6,14 +6,12 @@ closeMenuBtn.addEventListener('click', closeMenu);
 
 function openMenu() {
     document.querySelector('nav ul').style.display = 'flex';
-    // openMenuBtn.style.display = 'none';
     document.querySelector('body').style.overflowY = 'hidden';
     document.querySelector('.backdrop').style.display = 'block';
 }
 
 function closeMenu() {
     document.querySelector('nav ul').style.display = 'none';
-    // openMenuBtn.style.display = 'inline-block';
     document.querySelector('body').style.overflowY = 'auto';
     document.querySelector('.backdrop').style.display = 'none';
 }
